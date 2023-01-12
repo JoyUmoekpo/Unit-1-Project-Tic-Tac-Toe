ea65080fd1cd5e7d3480fff05eea15a3f17fbac3
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-12 17:38:25 -0600

    Part 3 Completed

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import Square from "./Square";
 import "./App.css";
 
 function App() {
-	
-	const string = "Welcome to my application!";
+	const string = "Welcome to my Tic Tac Toe application!";
+
+	const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+	const [player, setPlayer] = useState(true);
 
 	return (
 		<div className="App">
-			<Square propVar={string}/>
+			<Square propVar={string} />
+			<Square
+				squares={squares}
+				setSquares={setSquares}
+				player={player}
+				setPlayer={setPlayer}
+			/>
 		</div>
 	);
 }
 
-export default App;  
+export default App;
--- tic-tac-toe/src/Square.jsx
+++ tic-tac-toe/src/Square.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Square = (props) => {
-	return <div className="square">{props.propVar}</div>;
+	return <div className="square">{console.log(props.squares, props.player)}</div>;
 };
 
 export default Square;
